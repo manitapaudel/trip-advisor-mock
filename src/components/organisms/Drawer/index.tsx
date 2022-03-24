132bfe0a7cb3edfc2b0988114759e15fe12240ba
Manitajs <manita.paudel@ombryo.com>
2022-03-24 20:36:57 +0545

    Reorder imports, add animation to the drawer with transition, remove animation property

--- src/components/organisms/Drawer/index.tsx
+++ src/components/organisms/Drawer/index.tsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Link from "next/link";
 
+import { navItems } from "@/constants";
 import useClickOutside from "@/hooks/useOutsideClick";
 import XCircleIcon from "@/assets/icons/XCircleIcon";
-import { navItems } from "@/constants";
+import UserIcon from "@/assets/icons/UserIcon";
 import { H2 } from "@/components/atoms/Typography";
 import style from "./drawer.module.scss";
-import UserIcon from "@/assets/icons/UserIcon";
 
 type DrawerProps = {
   showModal: boolean;
   setShowDrawer(a: boolean): void;
   setShowModal(a: boolean): void;
+  className?: string;
 };
 
-const Drawer: React.FC<DrawerProps> = ({ setShowDrawer, setShowModal, showModal }) => {
+const Drawer: React.FC<DrawerProps> = ({ setShowDrawer, setShowModal, showModal, className }) => {
     const drawerRef = React.useRef<HTMLDivElement>(null);
   
     useClickOutside(drawerRef, () => setShowDrawer(false));
 
   return (
-    <div className={`${style.container} ${!showModal ? style.close : ""}`} ref={drawerRef}>
+    <div className={`${style.container} ${className}`} ref={drawerRef}>
       <H2>Menu</H2>
       <button className={style.closeButton} onClick={() => setShowDrawer(false)}>
         <XCircleIcon className={style.icon} />
